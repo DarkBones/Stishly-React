7b8b21b97b03f1984f6e7bac33f9816b163903ed
= <donkerbc@gmail.com>
2019-11-14 07:54:07 -0500

    Configure folder structure

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from '../../assets/images/logo';
+import '../../assets/css/App';
 
 function App() {
   return (
