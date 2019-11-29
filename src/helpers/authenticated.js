8cd04845fc4773db844c003306012f56e7b48a80
= <donkerbc@gmail.com>
2019-11-29 08:28:32 -0500

    Authenticated helper

--- src/helpers/authenticated.js
+++ src/helpers/authenticated.js
@@ -1,24 +1,37 @@
 import axios from 'axios';
 import { getJwt } from './jwt';
+import React, { Component } from 'react';
 
-export default () => {
-  const jwt = getJwt();
+class Authenticated extends Component {
+  constructor(props) {
+    super(props);
 
-  if (!jwt) {
-    return false;
+    this.state = {
+      authenticated: false
+    }
   }
-
-  axios.get("http://localhost:3001/api/v1/user/get_user/", {
-    headers: { Authorization: `Bearer ${jwt}` }
-  })
-    .then(() => {
-      return true;
-    }).catch(error => {
-      if (error.response && error.response.status == 401) {
-        localStorage.removeItem("jwt-token");
-      } else {
-        // TODO: Display message saying there was an unexpected error
-      }
-      return false;
-    })
 }
+
+export default Authenticated;
+
+// export default () => {
+//   const jwt = getJwt();
+
+//   if (!jwt) {
+//     return false;
+//   }
+
+//   axios.get("http://localhost:3001/api/v1/user/get_user/", {
+//     headers: { Authorization: `Bearer ${jwt}` }
+//   })
+//     .then(() => {
+//       return true;
+//     }).catch(error => {
+//       if (error.response && error.response.status == 401) {
+//         localStorage.removeItem("jwt-token");
+//       } else {
+//         // TODO: Display message saying there was an unexpected error
+//       }
+//       return false;
+//     })
+// }
