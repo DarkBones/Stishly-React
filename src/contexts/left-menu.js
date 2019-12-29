d47e30757ea576b06cc416c029956295b67e3180
= <donkerbc@gmail.com>
2019-12-28 19:34:04 -0500

    Bug fixes

--- src/contexts/left-menu.js
+++ src/contexts/left-menu.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default React.createContext({
   leftMenuEnabled: false,
   leftMenuExtended: true,
-  // enableLeftMenu: (enabled) => { },
+  enableLeftMenu: (enabled) => { },
   toggleLeftMenu: () => { },
   setMobile: () => { }
 });
