c36cf3b189c73a02beea17166106903f572d923a
= <donkerbc@gmail.com>
2020-01-02 10:51:02 -0500

    Get user accounts

--- src/components/Navigation/LeftMenu/LeftMenu.js
+++ src/components/Navigation/LeftMenu/LeftMenu.js
@@ -1,11 +1,61 @@
 import React, { Component } from 'react';
+import { getJwt } from '../../../helpers/jwt';
+import axios from 'axios';
 
 function LeftMenuSection(props) {
   return (
     <div className="section">
       <h4>{props.title}</h4>
+      <div>
+        {props.children}
+      </div>
     </div>
   );
+}
+
+class Accounts extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loading: true
+    }
+  }
+
+  componentDidMount() {
+    const jwt = getJwt();
+
+    axios.get("http://localhost:3001/api/v1/accounts/", {
+      headers: { Authorization: `Bearer ${jwt}` }
+    })
+      .then(response => {
+        console.log(response);
+        this.setState({
+          loading: false
+        });
+      })
+      .catch(error => {
+        // TODO: Handle error
+        console.log(error);
+      })
+  }
+
+  render() {
+    if (this.state.loading) {
+      return (
+        <React.Fragment>
+          {/* TODO: Use locales */}
+          <p>Loading ...</p>
+        </React.Fragment>
+      );
+    } else {
+      return (
+        <React.Fragment>
+          <p>Placeholder</p>
+        </React.Fragment>
+      );
+    }
+  }
 }
 
 class LeftMenu extends Component {
@@ -13,7 +63,9 @@ class LeftMenu extends Component {
   render() {
     return (
       <div className="container container-content">
-        <LeftMenuSection title="ACCOUNTS" />
+        <LeftMenuSection title="ACCOUNTS">
+          <Accounts />
+        </LeftMenuSection>
       </div>
     );
   }
